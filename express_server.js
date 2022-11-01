a1d84821584e9a72ba25e2513c4cb46516a65f75
Njoe00 <nick.james.joe@gmail.com>
2022-10-31 20:58:43 -0700

    updated to take in URLs and return back a message. Created function that returns back random string of numbers and letters

--- express_server.js
+++ express_server.js
@@ -1,13 +1,25 @@
+function generateRandomString() {};
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" }
@@ -18,6 +30,7 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
